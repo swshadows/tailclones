19b509a933bf50362a06ff0456863e3e4ee3de50
swshadows <joelson.jpl@gmail.com>
2022-05-28 19:34:11 -0400

    Adicionando projeto Facebook | Ajustando bug no projeto Spotify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
 module.exports = {
-  content: ["./docs/*.html"],
+  content: ["./src/*.html"],
   theme: {
     fontFamily: {
-      sans: ["BIZ UDPGothic", "sans-serif"],
+      sans: ["'BIZ UDPGothic'", "sans-serif"],
+      facebook: ["SFProDisplay-Regular", "sans-serif"],
     },
     extend: {
       colors: {
         "spotify-hover": "#18D760",
         "spotify-green": "#1ED760",
         "spotify-purple": "#2D46B9",
+        "facebook-gray": "#f0f2f5",
+        "facebook-blue": "#166fe5",
+        "facebook-green": "#36a420",
+        "facebook-links": "#8a8d91",
       },
+
+      borderWidth: {
+        "1px": "1px",
+      },
+
       width: (theme) => ({
         box: "80rem",
+        "facebook-footer": "980px",
       }),
 
       fontSize: (theme) => ({
@@ -19,7 +30,7 @@ module.exports = {
       }),
 
       backgroundImage: (theme) => ({
-        "spotify-theme": "url('../img/bursts.svg')",
+        "spotify-theme": "url('../img/spotify/bursts.svg')",
       }),
 
       backgroundSize: (theme) => ({
